cab4f1c2d9e782195b4f837cff1b1f9b88e6ec41
ksy316 <se316@gmail.com>
2020-10-08 14:13:40 +0900

    20201008 오후 2시 13분

--- mini/src/mini/Run.java
+++ mini/src/mini/Run.java
@@ -1,5 +1,11 @@
 package mini;
 
 public class Run {
-
+public static void main(String[] args) {
+		
+		// 1)실행할 메소드가 있는 클래스를 생성(new) 해야한다.
+		//[표현법] 클래스명 사용할이름 = new 클래스명();
+	System.out.println("멍청이");
+		
+}
 }
